4c6d52f7feb77df51198c67a02ffd987ff565d4d
PPP <xamidramazanob@gmail.com>
2025-06-25 11:03:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const btn=document.getElementById("btn");
+const karobka=document.getElementById("karobka");
+
+
+
+
+async function getData() {
+  const res = await fetch("https://ipinfo.io/json");
+  const data = await res.json();
+  chizish(data);
+}
+
+btn.addEventListener("click", () => {
+  getData();
+});
+
+function chizish(malumot) {
+  karobka.innerHTML = `
+    <p>Kenglik: ${malumot.loc.split(",")[0]}</p>
+    <p>Uzunlik: ${malumot.loc.split(",")[1]}</p>
+    <p>Mamlakat: ${malumot.country}</p>
+    <p>Shahar: ${malumot.city}</p>
+    <p>IP: ${malumot.ip}</p>
+    <p>Provayder: ${malumot.org}</p>
+    <iframe src="https://maps.google.com/maps?q=${malumot.loc.split(",")[0]},${malumot.loc.split(",")[1]}&z=14&output=embed"
+            frameborder="0" style="width:100%; height:300px;"></iframe>
+  `;
+}
